d68e9c8aff7a804bbfead67ee01351a3a67042ad
unknown <gustavo.grecco@hotmail.com>
2017-10-20 14:39:01 -0300

    Arquivos iniciais de python

---
+++ vogal.py
@@ -0,0 +1,32 @@
+def vogal(s):
+    if s == "a":
+        return True
+    if s == "e":
+        return True
+        
+    if s == "i":
+        return True
+
+    if s == "o":
+        return True
+
+    if s == "u":
+        return True
+
+    if s == "A":
+        return True
+        
+    if s == "E":
+        return True
+        
+    if s == "I":
+        return True
+        
+    if s == "O":
+        return True
+        
+    if s == "U":
+        return True
+    
+    else:
+        return False
