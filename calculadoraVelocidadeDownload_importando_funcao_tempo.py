4692722a83aadcfb928cf059f91d33725927a03c
ggrecco <32963507+ggrecco@users.noreply.github.com>
2017-11-17 20:20:27 -0200

    Add files via upload

---
+++ calculadoraVelocidadeDownload_importando_funcao_tempo.py
@@ -0,0 +1,16 @@
+import fun_Tempo
+
+def calcVel(k):
+    return k/8
+  
+i = 1
+while i != 0:
+    n = int(input("Velocidade contratada[(zero) para sair]: "))
+    t = (float(input("Tamano do arquivo em MegaBytes: ")))
+    segundos = t / calcVel(n)
+    fun_Tempo.calcTempo(segundos)
+    print("Velocidade máxima de Download {} MB/s\n".format(calcVel(n)))
+    i = n
+
+
+    
