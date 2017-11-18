263226ddfad7c1b6c8215cd8718c594efe2ad90c
Gustavo Grecco <gustavo.grecco@hotmail.com>
2017-11-18 14:26:14 -0200

    corrigido linha  frase

--- calculadoraVelocidadeDownload_importando_funcao_tempo.py
+++ calculadoraVelocidadeDownload_importando_funcao_tempo.py
@@ -6,7 +6,7 @@ def calcVel(k):
 i = 1
 while i != 0:
     n = int(input("Velocidade contratada[(zero) para sair]: "))
-    t = (float(input("Tamano do arquivo em MegaBytes: ")))
+    t = (float(input("Tamanho do arquivo em MegaBytes: ")))
     segundos = t / calcVel(n)
     fun_Tempo.calcTempo(segundos)
     print("Velocidade máxima de Download {} MB/s\n".format(calcVel(n)))
