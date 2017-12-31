42b2ae1600f2b10828a60c09c6d63930326bd471
gustavo grecco <gustavo.grecco@hotmail.com>
2017-12-31 00:06:40 -0200

    Aulas de validação

---
+++ zumbis/grandoArquivoHTML.py
@@ -0,0 +1,13 @@
+arquivo = open("ola.html", "w")
+arquivo.write('''<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+<head>
+<meta charset="utf-8">
+<title>Título da Página</title>
+</head>
+<body>
+<h1>olá!</h1>
+</body>
+</html>''')
+arquivo.close()
