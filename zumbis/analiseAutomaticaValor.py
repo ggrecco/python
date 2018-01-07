0b467465951c7046507620caef1f6cdc590c9756
gustavo grecco <gustavo.grecco@hotmail.com>
2018-01-07 17:30:52 -0200

    Analisa site com intervalo de tempo

--- zumbis/analiseAutomaticaValor.py
+++ zumbis/analiseAutomaticaValor.py
@@ -1,4 +1,6 @@
+#CUIDADO! programa para ataque DDoS
 import urllib.request
+import time
 preco = 99.99
 while preco > 4.74:
     pagina = urllib.request.urlopen("http://beans.itcarlow.ie/prices-loyalty.html")
@@ -7,4 +9,6 @@ while preco > 4.74:
     inicio = onde + 2
     fim = inicio + 4
     preco = float(texto[inicio:fim])
+    if preco >= 4.74:
+        time.sleep(10)
 print("Comprar! Preço: {:5.2f}".format(preco))
