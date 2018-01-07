ce7ed8df9d4e15ed6140d70e13de9acea3820911
gustavo grecco <gustavo.grecco@hotmail.com>
2018-01-07 17:06:01 -0200

    Captura html puro + localização de algum caractere para inicio da captua do dado

--- zumbis/capturaTextoInternet.py
+++ zumbis/capturaTextoInternet.py
@@ -1,6 +1,18 @@
 import urllib.request
+
 pagina = urllib.request.urlopen("http://beans.itcarlow.ie/prices.html")
+pagina2 = urllib.request.urlopen("http://beans.itcarlow.ie/prices-loyalty.html")
+
 texto = pagina.read().decode("utf8")
+texto2 = pagina2.read().decode("utf8")
+
 preco = texto[234:238]
+onde = texto2.find(">$")
+inicio = onde + 2
+fim = inicio + 4
+preco2 = texto2[inicio:fim]
+
 print(texto)
 print(preco)
+print("\n",texto2)
+print(preco2)
