200c4c853a2f6217830765ea0d40c63fba87217d
gustavo grecco <gustavo.grecco@hotmail.com>
2018-01-07 17:14:08 -0200

    testa se o valor selecionado

--- zumbis/capturaTextoInternet.py
+++ zumbis/capturaTextoInternet.py
@@ -6,13 +6,16 @@ pagina2 = urllib.request.urlopen("http://beans.itcarlow.ie/prices-loyalty.html")
 texto = pagina.read().decode("utf8")
 texto2 = pagina2.read().decode("utf8")
 
-preco = texto[234:238]
 onde = texto2.find(">$")
 inicio = onde + 2
 fim = inicio + 4
-preco2 = texto2[inicio:fim]
 
+preco = texto[234:238]
+preco2 = texto2[inicio:fim]
+preco3 = float(preco)
 print(texto)
 print(preco)
 print("\n",texto2)
 print(preco2)
+if preco3 > 6:
+    print("O preço está maior que R$ 6,00")
