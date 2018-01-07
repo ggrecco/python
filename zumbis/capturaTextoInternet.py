cbe612154808b39ef734e721a085b030b91bdf2a
gustavo grecco <gustavo.grecco@hotmail.com>
2018-01-07 16:42:59 -0200

    Captura html puro + posição específica determinada pelo programador

--- zumbis/capturaTextoInternet.py
+++ zumbis/capturaTextoInternet.py
@@ -1,4 +1,6 @@
 import urllib.request
 pagina = urllib.request.urlopen("http://beans.itcarlow.ie/prices.html")
 texto = pagina.read().decode("utf8")
+preco = texto[234:238]
 print(texto)
+print(preco)
