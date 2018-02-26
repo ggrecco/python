9d41a35ac784b9d7ac78026d84e648223d86ae71
Gustavo Grecco <gustavo.grecco@hotmail.com>
2018-02-25 22:49:34 -0300

    2248250218

---
+++ scrapy/cep.py
@@ -0,0 +1,12 @@
+import requests
+
+#Método post
+url = 'http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaCepEndereco.cfm'
+
+payload = {'relaxation':'94850620',
+            'tipoCEP':'ALL',
+            'semelhante':'N'}
+response = requests.post(url, data = payload)
+
+with open('correios.html', 'w') as f:
+    f.write(response.text)
