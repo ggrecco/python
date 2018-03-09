2f7c4a0fb108897d639098904a33c9f43ac7cd73
Gustavo Grecco <gustavo.grecco@hotmail.com>
2018-03-09 11:24:45 -0300

    criada novas pastas

--- scrapy/udemy/novatec/parte4.py
+++ scrapy/udemy/novatec/parte4.py
@@ -16,6 +16,14 @@ def post_http(url, nome_livro):
 		raise
 	return None
 
+def tratar_dados(lista_auxiliar):
+	preco = lista_auxiliar[6].replace('PreÃ§o: ','')
+	d = {'titulo':lista_auxiliar[0],
+		'url_capa': lista_auxiliar[7],
+		'url_produto': lista_auxiliar[8],
+		'preco':preco}
+	return d
+
 def parse_html(content):
 	soup = BeautifulSoup(content, 'lxml')
 
@@ -28,14 +36,34 @@ def parse_html(content):
 		f.write('\n\n\n')
 
 	f.close()"""
-	lista_produtos = []
+	lista_auxiliar = []
+	lista_produto = []
+	url = 'http://www.novatec.com.br/'
+	url_capa = ''
+	url_produto = ''
 	for produto in produtos:
+		tag_a = produto.find('a')
+		if tag_a:
+			if tag_a.next_element.next_element.name == 'img':
+				url_capa = "{0}{1}".format(url, tag_a.img.get('src'))
+				url_produto = "{0}{1}".format(url, tag_a.get('href'))
+				
+
 		for string in produto.stripped_strings:
 			if(string == 'Esgotado'):
 				continue
-			lista_produtos.append(string)
-	print(lista_produtos)
+			lista_auxiliar.append(string)
 
+		if(len(lista_auxiliar) > 6):
+			lista_auxiliar.append(url_capa)
+			lista_auxiliar.append(url_produto)
+			lista_produto.append(tratar_dados(lista_auxiliar))
+		del lista_auxiliar[:]
+
+	if(len(lista_produto) > 0):
+		del lista_produto[0]
+
+	return lista_produto
 
 
 
@@ -44,6 +72,7 @@ if __name__ == '__main__':
 	#nome_livro = input("nome do livro: ")
 	nome_livro = 'redes de computadores'
 	r = post_http(url, nome_livro)
-	
+	lista_produto = []
 	if r:
-		parse_html(r.text)
+		lista_produto = parse_html(r.text)
+	print(lista_produto)
