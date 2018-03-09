a2205af57e0eabba9bce6875dd57f152f59fef3e
Gustavo Grecco <gustavo.grecco@hotmail.com>
2018-03-09 11:26:44 -0300

    trocado nome da pasta

--- scrapy/udemy/download_imagem.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'http://st2.depositphotos.com/1514397/5893/i/450/depositphotos_58937687-Woman-putting-roller-skates.jpg'
-
-r = requests.get(url)
-
-with open('img.jpg', 'wb') as f:
-	f.write(r.content)
