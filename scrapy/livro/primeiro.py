b9ce4cd63741d8604604a8362a9ebcb750452d36
Gustavo Grecco <gustavo.grecco@hotmail.com>
2018-03-15 12:40:44 -0300

    usando BeautifulSoup

--- scrapy/livro/primeiro.py
+++ scrapy/livro/primeiro.py
@@ -1,5 +1,7 @@
 from urllib.request import urlopen
-
+from bs4 import BeautifulSoup
 
 html = urlopen("http://pythonscraping.com/pages/page1.html")
-print(html.read())
+#print(html.read()) #utilizando o reqest
+bsObj = BeautifulSoup(html.read())
+print(bsObj.h1)
