813137d390e71e6d76ef20ccc4c46e8a634936c1
Gustavo Grecco <gustavo.grecco@hotmail.com>
2018-03-17 11:35:26 -0300

    codigos da prova basica

---
+++ curso_em_video/prova.py
@@ -0,0 +1,25 @@
+'''from random import randint
+num = randint(1, 6)
+res = num // 2
+print(res)clear
+################
+texto = 'Tres Pratos de Trigo para Tigres Tristes'
+total = texto.upper().count(texto[0])
+print(total)
+##################
+a = 4
+b = 3
+c = 2
+d = a + b * c
+e = d % c + 1
+print("{} e {}".format(d, e))
+############################
+'''
+from random import choice
+n = [2, 5, 9, 1, 4]
+res = choice(n) % n[0]
+print(res)
+
+
+
+############################
