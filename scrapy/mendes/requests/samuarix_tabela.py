691205d698a1f0c9b503d2eb92b2ee16eae50f73
Gustavo Grecco <gustavo.grecco@hotmail.com>
2018-03-18 18:18:54 -0300

    #scrapy e lista de dados da tabela

--- scrapy/mendes/requests/samuarix_tabela.py
+++ scrapy/mendes/requests/samuarix_tabela.py
@@ -1,1 +1,7 @@
+#grava e mum arquivo .cvs
 from samuraix import lista_episodios
+
+with open('episodios.cvs', 'w') as _file:
+    _file.write('Temporada; Episódio; Nome\n')
+    for ep in lista_episodios:
+        _file.write('{};{};{}\n'.format(ep.temporada, ep.episodio, ep.nome))
