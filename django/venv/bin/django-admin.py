f396209e13fe881b4665e12599e861cd672be348
Gustavo Grecco <gustavo.grecco@hotmail.com>
2018-03-28 12:06:35 -0300

    implementada entries(livro)

--- django/venv/bin/django-admin.py
+++ django/venv/bin/django-admin.py
@@ -1,6 +1,5 @@
 #!/home/grecco/Documentos/Python/django/venv/bin/python3
-# EASY-INSTALL-DEV-SCRIPT: 'Django==2.1.dev20180326173423','django-admin.py'
-__requires__ = 'Django==2.1.dev20180326173423'
-__import__('pkg_resources').require('Django==2.1.dev20180326173423')
-__file__ = '/home/grecco/Downloads/django/django/bin/django-admin.py'
-exec(compile(open(__file__).read(), __file__, 'exec'))
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
