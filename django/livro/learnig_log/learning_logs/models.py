1228ba82998436377ad367cea30a722937e4103a
Gustavo Grecco <gustavo.grecco@hotmail.com>
2018-03-28 11:02:45 -0300

    migrations para learning_logs

--- django/livro/learnig_log/learning_logs/models.py
+++ django/livro/learnig_log/learning_logs/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Topic(models.Model):
     """ Um assunto sobre o qual o usuŕio está aprendendo. """
-    text = models.CharField(max_length=200)#usado quando queremos armazenar uma quantidade pequena de texto
-    date_added = models.DateTimeField(auto_now_add=True)
+    text = models.CharField(max_length=200)#usado para armazenar uma quantidade pequena texto
+    date_added = models.DateTimeField(auto_now_add=True)#armazena data da modificação
 
     def __str__(self):
         """Devolve uma representação em string do modelo."""
