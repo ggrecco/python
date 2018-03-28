a3d6c3c14599809604de125fa5411846b074ee85
Gustavo Grecco <gustavo.grecco@hotmail.com>
2018-03-28 08:38:46 -0300

    atualizado models

--- django/livro/learnig_log/learning_logs/models.py
+++ django/livro/learnig_log/learning_logs/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Topic(models.Model):
     """ Um assunto sobre o qual o usuŕio está aprendendo. """
-    text = models.CharField(max_length=200)
+    text = models.CharField(max_length=200)#usado quando queremos armazenar uma quantidade pequena de texto
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
