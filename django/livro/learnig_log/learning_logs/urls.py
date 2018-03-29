fb7543580ecec707ff0588941d99c5db1c0a21d4
Gustavo Grecco <gustavo.grecco@hotmail.com>
2018-03-29 09:50:06 -0300

    adiconado improtação django.conf.urls para teste

--- django/livro/learnig_log/learning_logs/urls.py
+++ django/livro/learnig_log/learning_logs/urls.py
@@ -1,5 +1,6 @@
 """Define padrões de URL para learning_logs."""
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
@@ -7,5 +8,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     #Mostra todos os assuntos
     path('topics/', views.topics, name='topics'),
+    path('new_topic', views.new_topic, name='new_topic'),
+    #adicionar uma nova entrada
+    url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry')
 
 ]
--- django/livro/learnig_log/learning_logs/views.py
+++ django/livro/learnig_log/learning_logs/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .forms import TopicForm
 from .models import Topic
+from .forms import TopicForm, EntryForm
 
 def index(request):
     """A página inicial de Learning Log"""
@@ -10,3 +14,34 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
+def new_topic(request):
+    """Adiciona um  novo assunto"""
+    if request.method != 'POST':
+        #Cria um formulário em branco
+        form = TopicForm()
+    else:
+        #processa os dados
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+    context = {'form' : form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Acrescenta uma nova entrada para um assunto em particular."""
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        #Nenhum dado submetido; cria um formulario em branco
+        form = EntryForm()
+    else:
+        #Dados de POST submetidos; processa os Dados
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+    context = {'topic': topic, 'form' : form}
