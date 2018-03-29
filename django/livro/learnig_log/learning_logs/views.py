0767e03cd6e2307fb88fd56e36bf2d6eeae854e9
Gustavo Grecco <gustavo.grecco@hotmail.com>
2018-03-29 09:53:48 -0300

    modificado new_entry de views.py

--- django/livro/learnig_log/learning_logs/views.py
+++ django/livro/learnig_log/learning_logs/views.py
@@ -45,3 +45,4 @@ def new_entry(request, topic_id):
             new_entry.save()
             return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
     context = {'topic': topic, 'form' : form}
+    return render(request, 'learning_logs/new_entry.html', context)
