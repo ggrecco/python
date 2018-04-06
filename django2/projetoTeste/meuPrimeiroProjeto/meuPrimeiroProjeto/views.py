213d56d2163f6b3c1c9695251424cfac65422fd6
Gustavo Grecco <gustavo.grecco@hotmail.com>
2018-04-06 15:15:40 -0300

    criado banco de dados e acesso de adm

--- django2/projetoTeste/meuPrimeiroProjeto/meuPrimeiroProjeto/views.py
+++ django2/projetoTeste/meuPrimeiroProjeto/meuPrimeiroProjeto/views.py
@@ -5,3 +5,26 @@ def hello(request):
 
 def articles(request, year):
     return HttpResponse('O ano enviado foi: ' + str(year))
+
+
+def lerDobanco(nome):
+    lista_nomes = [
+        {'nome': 'Karim', 'idade':'35'},
+        {'nome': 'Gustavo', 'idade':'32'},
+        {'nome': 'Darth', 'idade':'1'},
+        {'nome': 'Vader', 'idade':'1'}
+    ]
+
+    for pessoa in lista_nomes:
+        if pessoa['nome'] == nome:
+            return pessoa
+    else:
+        return {'nome':'', 'idade':'0'}
+
+def fname(request, nome):
+    result = lerDobanco(nome)
+    # print(result)
+    if result['idade'] != '0':
+        return HttpResponse('A pessoa '+ result['nome'] +' encontrada tem ' + result['idade'] + ' anos.')
+    else:
+        return HttpResponse('Pessoa não encontrada')
