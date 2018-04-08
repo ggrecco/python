9c7a0b5b990c41be602d9269ebf289b246a27ebb
Gustavo Grecco <gustavo.grecco@hotmail.com>
2018-04-08 14:14:08 -0300

    CRud, criar e ler prontos

--- django2/projetoTeste/meuPrimeiroProjeto/clientes/urls.py
+++ django2/projetoTeste/meuPrimeiroProjeto/clientes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import persons_list, persons_new
 
 urlpatterns = [
-    path('list/', persons_list, name='persons_list'),
-    path('new/', persons_new, name='persons_new'),
+    path('list/', persons_list, name='person_list'),
+    path('new/', persons_new, name='person_new'),
 
 ]
