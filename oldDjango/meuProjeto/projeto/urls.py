c74d0862cd07946faf2834cf7f3590ddba8dda15
Gustavo Grecco <gustavo.grecco@hotmail.com>
2018-04-10 12:07:42 -0300

    a

--- oldDjango/meuProjeto/projeto/urls.py
+++ oldDjango/meuProjeto/projeto/urls.py
@@ -18,4 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^usuario/', include('usuario.urls', namespace='usuario')),
 ]
--- oldDjango/meuProjeto/usuario/models.py
+++ oldDjango/meuProjeto/usuario/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class usuario(models.Model):
+    nome = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    password = models.PasswordField(max_length=200)
+
+    def __str__(self):
+        return self.text
