6d87bb04cf3506a03101d846e9d720b23cf5ebab
Gustavo Grecco <gustavo.grecco@hotmail.com>
2018-04-11 12:49:22 -0300

    base CRUD

--- django2/meuProjeto/cve/cve/urls.py
+++ django2/meuProjeto/cve/cve/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import index
+from clientes import urls as clientes_urls
 
 urlpatterns = [
     path('index/', index),
     path('admin/', admin.site.urls),
+    path('cliente/', include(clientes_urls)),
 ]
