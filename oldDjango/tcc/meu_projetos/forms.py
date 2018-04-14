eaefa55cb942e17bdbcbbc3630505e3e6282d2d1
Gustavo Grecco <gustavo.grecco@hotmail.com>
2018-04-14 11:52:46 -0300

    retirada partes do codigo não utilizada

--- oldDjango/tcc/meu_projetos/forms.py
+++ oldDjango/tcc/meu_projetos/forms.py
@@ -1,16 +1,8 @@
 from django import forms
-from .models import Servidor, Entrada
+from .models import Servidor
 
 class ServidorForm(forms.ModelForm):
     class Meta:
         model = Servidor
         fields = ['nome']
         labels = {'nome' : ''}
-'''
-class EntradaForm(forms.ModelForm):
-    class Meta:
-        model = Entrada
-        fields = ['produto', 'comentario']
-        labels = {'produto':produto , 'comentario':comentario}
-        widgets = {'produto':produto , 'comentario': forms.Textarea(attrs={'cols': 80})}
-'''
