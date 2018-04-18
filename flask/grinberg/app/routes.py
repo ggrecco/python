593c5f5e1b13a789dff67049b51ab05c96974dbf
Gustavo Grecco <gustavo.grecco@hotmail.com>
2018-04-18 08:53:07 -0300

    fim capitulo 2(renderizando base.html)

--- flask/grinberg/app/routes.py
+++ flask/grinberg/app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello Word!"
+    user = {'username': 'Gustavo'}
+    posts    = [
+        {
+            'autor':{'username': 'Gustavo'},
+            'body':'Um belo dia para estudar!'
+        },
+        {
+            'autor':{'username': 'Karim'},
+            'body': 'Piratas do caribe é um BAITA filmão.'
+        }
+    ]
+    return render_template('index.html', title='Título com acentuação', user=user, posts=posts)
