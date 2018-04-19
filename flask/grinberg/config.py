0d4f583a04db4645b0eaec1e468dc8d6ed24fedb
Gustavo Grecco <gustavo.grecco@hotmail.com>
2018-04-19 13:16:12 -0300

    capitulo 4 - shell e banco de dados

--- flask/grinberg/config.py
+++ flask/grinberg/config.py
@@ -1,4 +1,7 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 #configura SECRET_KEY para evitar ataques CSRF
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'voce-nao-vai-passar'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
