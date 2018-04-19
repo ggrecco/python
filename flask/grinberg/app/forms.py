4cda0fcfaa7770a02534468bec9848e92ca2e0d4
Gustavo Grecco <gustavo.grecco@hotmail.com>
2018-04-19 16:59:06 -0300

    capitulo 5, login de usuário e registro de usuários

--- flask/grinberg/app/forms.py
+++ flask/grinberg/app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
 
 class LoginForm(FlaskForm):
     # username = StringField('Username', validators=[DataRequired()])
@@ -11,3 +12,20 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Lembrar')
     # submit = SubmitField('Sign in')
     submit = SubmitField('Entrar')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Usuário', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    password2 = PasswordField('Repita a Senha', validators=[DataRequired(), EqualTo('password')] )
+    submit = SubmitField('Registar')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Por Favor use outro nome de usuário.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Por Favor use outro e-mail.')
