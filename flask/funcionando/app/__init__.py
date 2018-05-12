46c9943c1a4c96cc09c6c11c2e2572d5fbc42f91
Gustavo Grecco <gustavo.grecco@hotmail.com>
2018-05-11 22:43:59 -0300

    iniciando com celery

--- flask/funcionando/app/__init__.py
+++ flask/funcionando/app/__init__.py
@@ -4,28 +4,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
-from celery import Celery
-
-def make_celery(app):
-    celery = Celery(app.import_name, backend=app.config['CELERY_RESULT_BACKEND'],
-                    broker=app.config['CELERY_BROKER_URL'])
-    celery.conf.update(app.config)
-    TaskBase = celery.Task
-    class ContextTask(TaskBase):
-        abstract = True
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-    celery.Task = ContextTask
-    return celery
-
+from flask_celery import make_celery
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379'
+app.config['CELERY_BROKER_URL'] = 'amqp://localhost//'
+app.config['CELERY_RESULT_BACKEND'] = '/home/ggrecco/Documentos/python/flask/funcionando/app.db'
 
-celery= make_celery(app)
+celery = make_celery(app)
 celery.conf.update(app.config)
 
 db = SQLAlchemy(app)
