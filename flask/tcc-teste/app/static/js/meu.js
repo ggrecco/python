4936cbb110c38a6571ab6db68500f03a0d7e75b1
Gustavo Grecco <gustavo.grecco@hotmail.com>
2018-07-07 23:14:36 -0300

    quase pronto

--- flask/tcc-teste/app/static/js/meu.js
+++ flask/tcc-teste/app/static/js/meu.js
@@ -13,6 +13,14 @@ function clickLoad(){
   document.getElementById("carregando").style.display = "block";
 }
 
+function imprimir(){
+  alert('Seu arquivo será preparado em nova aba!');
+}
+
+function redireciona(){
+  alert('Você será direcionado para o site www.cvedetails.com');
+}
+
 // criei uma função css que deixa sempre a tela de load e esconte o conteudo da pagina.
 // criei função javaScript que retira essa tela e carrega o conteúdo.
 // criei uma outra função para que a cada click seja carregada a tela de laod;
