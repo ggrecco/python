49d403420337a27db8812af17c07102d0ecbd285
Gustavo Grecco <gustavo.grecco@hotmail.com>
2018-08-17 16:05:38 -0300

    backup

--- flask/tccbackup/app/static/js/teste.js
+++
@@ -1,8 +0,0 @@
-var vals;
-$('#bt-ok').click(function() {
-    vals = [];
-    $("input[type='checkbox']:checked").each(function(){
-        vals.push($(this).prop('name'));
-    });
-    $("#text").val(vals.join(', '));
-});
