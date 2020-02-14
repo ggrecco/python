43594cbe3417a2adbef76ac5969847c0e5859e33
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-02-14 16:45:29 -0300

    conexão com a impressora padrão e envio da lista

--- scrapy/wolkers/primeiro/printImp.py
+++ scrapy/wolkers/primeiro/printImp.py
@@ -1,11 +1,18 @@
 from win32printing import Printer
+import pandas as pd
 
-font = {
-    "height": 32,
-}
-font2 = {
-    "height": 12,
-}
-with Printer(linegap=1) as printer:
-    printer.text("HMGV123", font_config=font)
-    printer.text("192.168.X.X", font_config=font2)
+df = pd.read_excel("teste_ip.xls")
+
+i = 0
+
+# font = {
+#     "height": 24,
+# }
+# font2 = {
+#     "height": 10,
+# }
+while i < len(df) :
+    with Printer(linegap=2) as printer:
+        printer.text(df['PC'][i], align="center", font_config={"height": 24})
+        printer.text(df['IP'][i], align="center", font_config={"height":10})
+        i = i + 1
