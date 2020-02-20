1ca4b1116e53a461894b4b59d33ddae323c6acce
Gustavo Grecco <gustavo.grecco@hotmail.com>
2020-02-20 11:04:26 -0300

    iniciando um grid

--- Janela/hello.py
+++ Janela/hello.py
@@ -1,7 +1,16 @@
 from tkinter import *
 
 root = Tk()
-myLabel = Label(root, text="hello world")
-myLabel.pack()
+
+# label
+myLabel1 = Label(root, text="hello world").grid(row=0, column=0)
+myLabel2 = Label(root, text="Eu sou o Gustavo").grid(row=1, column=1)
+
+# grid
+# myLabel1.grid(row=0, column=0)
+# myLabel2.grid(row=1, column=1)
+
+
+# myLabel.pack()
 
 root.mainloop()
